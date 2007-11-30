fe4f7e4d903caa4ed323438a121066a4dc1ea9c9
Nikita Maruniak <nikita.marunyak@gmail.com>
2007-11-30 11:59:48 +0000

    Ticket: #12

---
+++ src/apps/ConsoleXMLValidator/Sources/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace Toolkit.ConsoleXMLValidator
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			if( args.Length < 1 || args.Length > 2 ) {
+				Console.WriteLine( "Usage: Toolkit.ConsoleXMLParser xmlfile [schemafile]" );
+				return;
+			}
+
+			Validator validator = new Validator( args[ 0 ],
+				delegate( string msg) { Console.WriteLine( msg + "\n" ); } );
+
+			if( args.Length == 2 )
+				validator.Schema = args[ 1 ];
+
+			validator.Validate();
+
+			//Console.WriteLine( "..." ); Console.ReadLine();
+		}
+	}
+}
